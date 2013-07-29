591dc178380b9d54dc55576994d25c140657d1ee
erwinschens <erwinschens@uni-koblenz.de>
2013-07-29 23:43:33 +0200

    add new ruby on rails test project in resources

---
+++ eclipse/megal/src/test/resources/files/megalproject/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Megalproject::Application.config.secret_token = '74ec1f575739f7237e600d6a39b17b6158a4c73c8f0b573cdbf90a312a73977712f4a9d0417fe98f7834765c48f58915f7add67c870c02086b1e5f4f8015651d'
