72929ea550668ce55947b81d1c3ea27a8d306802
jan <jan-hendrikborth@Hennys-MacBook-Pro.local>
2013-07-31 23:44:37 +0200

    added github download

---
+++ eclipse/JastAddMegal/src/util/Net.java
@@ -0,0 +1,45 @@
+package util;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+
+public class Net {
+
+	public static void safeFileFromURI(File output, URI input){
+		BufferedInputStream in = null;
+    	FileOutputStream fout = null;
+    	try
+    	{
+    		in = new BufferedInputStream(new URL(input.toString()).openStream());
+    		fout = new FileOutputStream(output);
+    		byte data[] = new byte[1024];
+    		int count;
+    		while ((count = in.read(data, 0, 1024)) != -1)
+    		{
+    			fout.write(data, 0, count);
+    		}
+    		
+    	} catch(Exception e){
+    		e.printStackTrace();
+    	}
+    	finally{
+    		if (in != null)
+				try {
+					in.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+    		if (fout != null)
+				try {
+					fout.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+    	}
+	}
+	
+}
