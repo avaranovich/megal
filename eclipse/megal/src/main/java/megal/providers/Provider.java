08bc3eaf30fdb29102ca906f06d65c9903092ce6
erwinschens <erwinschens@uni-koblenz.de>
2013-08-07 11:32:11 +0200

    added provider2

---
+++ eclipse/megal/src/main/java/megal/providers/Provider.java
@@ -0,0 +1,88 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package megal.providers;
+
+import com.google.gson.Gson;
+import com.google.gson.internal.StringMap;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import megal.relationships.FileElementOfLanguage;
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.service.RepositoryService;
+
+/**
+ *
+ * @author erwinschens
+ */
+public class Provider {
+    
+    private URI url;
+    
+    public Provider(URI url){
+        this.url = url;
+        
+    }
+    
+    public List<URI> getUris(){
+       if(this.url.toString().contains("github")){
+           return listAllFilesFromDirectory(url.toString());
+       }
+       return null;
+    }
+    
+     /**
+     * Helper method to retreive all files that are contained in a directory
+     * needed for Files+ - Relationship
+     *
+     * @param gitHubUrl
+     * @return
+     */
+    private List<URI> listAllFilesFromDirectory(String gitHubUrl) {
+        List<URI> files = new ArrayList<URI>();
+        try {
+
+            RepositoryService repositoryService = new RepositoryService();
+            Repository repo = repositoryService.getRepository("avaranovich", "megal");
+
+            URLConnection urlConnection = new URL(repo.getUrl().concat("/contents/").concat(gitHubUrl)).openConnection();
+
+            BufferedReader rd = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            String line;
+            String jsonAsString = "";
+            while ((line = rd.readLine()) != null) {
+                jsonAsString = line;
+            }
+
+            List<StringMap> jsonJavaRootObject = new Gson().fromJson(jsonAsString, List.class);
+
+            //iterate over result
+            for (StringMap sm : jsonJavaRootObject) {
+                if (sm.get("type").equals("file")) {
+                    try {
+                        files.add(new URI("https://raw.github.com/avaranovich/megal/master/" + sm.get("path")));
+                    } catch (URISyntaxException ex) {
+                        Logger.getLogger(Provider.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(FileElementOfLanguage.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return files;
+
+    }
+    
+}
