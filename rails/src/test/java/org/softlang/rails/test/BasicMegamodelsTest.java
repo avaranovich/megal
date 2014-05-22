6ee10785cfba1d8b1ff1d97e354607f06664d05b
Andrei Varanovich <dotnetby@gmail.com>
2014-05-22 14:58:57 +0200

    Added test

---
+++ rails/src/test/java/org/softlang/rails/test/BasicMegamodelsTest.java
@@ -0,0 +1,20 @@
+package org.softlang.rails.test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class BasicMegamodelsTest extends BaseTest {
+
+    @Before
+    public void setUp() {
+        super.setUp();
+    }
+
+    @Test
+    public void Foo(){
+        assertEquals(true, true);
+    }
+}
