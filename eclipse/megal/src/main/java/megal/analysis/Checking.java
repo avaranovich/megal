0d4b33ad07d5b077f3644a5d7bd4101646528190
Erwin Schens <erwinschens@uni-koblenz.de>
2014-05-22 14:14:14 +0200

    added bitbucket provider and changed the tool to take arguments as input to run megal as a jar

--- eclipse/megal/src/main/java/megal/analysis/Checking.java
+++ eclipse/megal/src/main/java/megal/analysis/Checking.java
@@ -1,6 +1,7 @@
 package megal.analysis;
 
 import static megal.Context.eventBus;
+
 import megal.events.NotWellFormedRelationshipDetected;
 import megal.events.RelationshipEvaluationFailed;
 import megal.events.RelationshipEvaluationStarted;
@@ -12,30 +13,31 @@ import megal.relationships.core.Relationship;
 /*
  * Checks all the relationships in the megamodel.
  */
-public class Checking extends Visitor { 
+public class Checking extends Visitor {
 
-	public void visit(RDecl decl) {
-		Relationship<?,?> rel = decl.getRelationship();
-		
-		if (!rel.wellFormed()){
-			eventBus.post(new NotWellFormedRelationshipDetected(decl));
-			return;
-		}
-		
-		// check if the relationship is "weak"
-		if (rel.getClass().isAnnotationPresent(WeakRel.class)){
-			// we don't evaluate it
-			eventBus.post(new RelationshipEvaluationStarted(rel.getLeft(), rel.getRight(), rel));
-		}
-		else{
-			System.out.println(String.format("Evaluating relationship %s %s : %s using %s", 
-					rel.getLeft(), rel.getTypeDecl().getName(), rel.getRight(), rel.getSrc()));
-			
-			eventBus.post(new RelationshipEvaluationStarted(rel.getLeft(), rel.getRight(), rel));
-			boolean res = rel.evaluate();
-			if (res == false){
-				eventBus.post(new RelationshipEvaluationFailed(rel.getLeft(), rel.getRight(), rel));
-			}
-		}		
-	}
+    public void visit(RDecl decl) {
+        Relationship<?, ?> rel = decl.getRelationship();
+
+        if (rel != null && !rel.wellFormed()) {
+            eventBus.post(new NotWellFormedRelationshipDetected(decl));
+            return;
+        }
+
+        // check if the relationship is "weak"
+        if (rel != null) {
+            if (rel.getClass().isAnnotationPresent(WeakRel.class)) {
+                // we don't evaluate it
+                eventBus.post(new RelationshipEvaluationStarted(rel.getLeft(), rel.getRight(), rel));
+            } else {
+                System.out.println(String.format("Evaluating relationship %s %s : %s using %s",
+                        rel.getLeft(), rel.getTypeDecl().getName(), rel.getRight(), rel.getSrc()));
+
+                eventBus.post(new RelationshipEvaluationStarted(rel.getLeft(), rel.getRight(), rel));
+                boolean res = rel.evaluate();
+                if (res == false) {
+                    eventBus.post(new RelationshipEvaluationFailed(rel.getLeft(), rel.getRight(), rel));
+                }
+            }
+        }
+    }
 }
--- eclipse/megal/src/main/java/megal/providers/BitbucketProvider.java
+++ eclipse/megal/src/main/java/megal/providers/BitbucketProvider.java
@@ -1,30 +1,142 @@
 package megal.providers;
 
-import java.net.URI;
+import com.google.gson.Gson;
+import com.google.gson.internal.StringMap;
+import com.typesafe.config.Config;
+import megal.Context;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.codec.binary.Base64;
 
 //TODO: needs to be implemented
 public class BitbucketProvider implements IVCSProvider {
 
     private URI url;
+    private static String BITBUCKET_BASE_URL = "https://api.bitbucket.org/1.0/repositories";
 
-    public BitbucketProvider(URI url){
+    public BitbucketProvider(URI url) {
         this.url = url;
+    }
 
+    /**
+     * Helper method to retreive all files that are contained in a directory
+     * needed for Files+ - Relationship
+     *
+     * @param bitbucketURL
+     * @return
+     */
+    private List<URI> listAllFilesFromDirectory(String bitbucketURL) {
+        if (!isRepoValid()) {
+            return null;
+        } else {
+            List<URI> files = new ArrayList<URI>();
+            Map jsonJavaRootObject = new Gson().fromJson(getContent(), Map.class);
+            //check if uri is path to file or to directory
+            try {
+                if (jsonJavaRootObject.containsKey("files")) {
+                    for (StringMap f : (List<StringMap>) jsonJavaRootObject.get("files")) {
+                        files.add(new URI(createURLString() + f.get("path")));
+                    }
+                } else {
+                    files.add(new URI(createURLString() + jsonJavaRootObject.get("path")));
+                }
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+            return files;
+        }
+
+    }
+
+    private String createURLString() {
+        Config conf = Context.config;
+        List<Config> rels = (List<Config>) conf.getConfigList("repos");
+        String repo = null;
+        String username = null;
+        String branch = null;
+
+        for (Config c : rels) {
+            repo = c.getString("repo");
+            username = c.getString("username");
+            branch = c.getString("branch");
+        }
+        return (BITBUCKET_BASE_URL
+                + "/" + username
+                + "/" + repo
+                + "/src/"
+                + branch
+                + "/");
+    }
+
+    private boolean isRepoValid() {
+        return authenticateAndReturnConnection() != null ? true : false;
+    }
+
+    private URLConnection authenticateAndReturnConnection() {
+
+        Config conf = Context.config;
+        List<Config> rels = (List<Config>) conf.getConfigList("repos");
+        String password = null;
+        String username = null;
+        for (Config c : rels) {
+            username = c.getString("username");
+            if (c.hasPath("password")) {
+                password = c.getString("password");
+            }
+        }
+
+        String authString = username + ":" + password;
+        byte[] authEncBytes = Base64.encodeBase64(authString.getBytes());
+        String authStringEnc = new String(authEncBytes);
+        try {
+            URL url = new URL(createURLString());
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.setRequestProperty("Authorization", "Basic " + authStringEnc);
+            return urlConnection;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public List<URI> getUris() {
-        return null;
+        return listAllFilesFromDirectory(url.toString());
     }
 
     @Override
     public URI getUri() {
-        return null;
+        return listAllFilesFromDirectory(url.toString()).get(0);
     }
 
     @Override
     public String getContent() {
-        return null;
+        String content = null;
+        try {
+            URLConnection urlConnection = authenticateAndReturnConnection();
+            InputStream is = urlConnection.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            int numCharsRead;
+            char[] charArray = new char[1024];
+            StringBuffer sb = new StringBuffer();
+            while ((numCharsRead = isr.read(charArray)) > 0) {
+                sb.append(charArray, 0, numCharsRead);
+            }
+            content = sb.toString();
+
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return content;
     }
 }
