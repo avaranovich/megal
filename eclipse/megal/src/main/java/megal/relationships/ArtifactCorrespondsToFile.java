f1c9a147af9ee0fed15fcb1d6200f7cb1b2ae091
Erwin Schens <erwinschens@uni-koblenz.de>
2014-05-28 12:09:57 +0200

    added checker to relationship

--- eclipse/megal/src/main/java/megal/relationships/ArtifactCorrespondsToFile.java
+++ eclipse/megal/src/main/java/megal/relationships/ArtifactCorrespondsToFile.java
@@ -1,9 +1,13 @@
 package megal.relationships;
 
+import java.util.List;
+import megal.checkers.Fragment;
+import megal.checkers.RequestControllerChecker;
 import megal.entities.Artifact;
 import megal.entities.File;
 import megal.model.RTypeDecl;
 import megal.relationships.core.correspondsTo;
+import megal.trivia.Pair;
 
 public class ArtifactCorrespondsToFile extends correspondsTo<Artifact, File> {
 
@@ -11,10 +15,18 @@ public class ArtifactCorrespondsToFile extends correspondsTo<Artifact, File> {
         super(first, second, rTypeDecl);
     }
 
-
     @Override
     public boolean evaluate() {
-        first.getResource();
-        return true;
+        RequestControllerChecker requestChecker = new RequestControllerChecker(getResorucePath("/rails/utils/controller_fragments.json"), getResorucePath("/rails/utils/routes.json"));
+        Pair<Boolean, List<Pair<Fragment, Fragment>>> c_pair = requestChecker.check(first.getResource(), second.getResource());
+        if (c_pair.first) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private String getResorucePath(String relPath) {
+        return this.getClass().getResource(relPath).getPath();
     }
 }
